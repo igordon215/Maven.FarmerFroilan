bdc3bad7550d0a56c7dec50bf05cf5ee7a995801
Josue <josue@zipcodersmbp75.lan>
2024-07-27 11:25:08 -0400

    33testsDone

--- src/test/java/com/zipcodewilmington/froilansfarm/VehicleTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/VehicleTest.java
@@ -1,15 +1,63 @@
 package com.zipcodewilmington.froilansfarm;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class VehicleTest {
 
     @Test
-    public void vehicleIsRideable(){
-//        Vehicle tractor = new Vehicle();
-// im testing vehicle test
-
+    public void vehicleIsRideableTest(){
+        Vehicle tractor = new Tractor();
+        boolean actual = tractor instanceof Rideable;
+        Assert.assertTrue(actual);
     }
+
+    @Test
+    public void tractorHarvestCropTest(){
+        Vehicle tractor = new Tractor();
+        Crop tomatoPlant = new TomatoPlant();
+        Crop actual = tractor.harvest(tomatoPlant);
+        Assert.assertTrue(actual instanceof Crop);
+    }
+
+    @Test
+    public void vehicleIsNoiseMakerTest(){
+        Tractor tractor = new Tractor();
+        boolean actual = tractor instanceof NoiseMaker;
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void vehicleMakesNoiseTest(){
+        Tractor tractor = new Tractor();
+        String actual = tractor.makeNoise();
+        String expected = "Vroom vroom!";
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void tractorInstanceOfVehicle(){
+        Tractor tractor = new Tractor();
+        boolean actual = tractor instanceof Vehicle;
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void tractorIsFarmVehicleTest(){
+        Tractor tractor = new Tractor();
+        boolean actual = tractor instanceof FarmVehicle;
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void farmVehicleInstanceVehicleTest(){
+        Tractor tractor = new Tractor();
+        boolean actual = tractor instanceof Vehicle;
+        Assert.assertTrue(actual);
+    }
+
+
+
 
 
 }
