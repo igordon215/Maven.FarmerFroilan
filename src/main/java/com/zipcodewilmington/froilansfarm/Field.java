3f03374fbddd0fbed3dca1dfe71bfe595efc192a
Josue <josue@zipcodersmbp75.lan>
2024-07-27 16:56:44 -0400

    73tests Need to go line coverage up

--- src/main/java/com/zipcodewilmington/froilansfarm/Field.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Field.java
@@ -4,14 +4,5 @@ import java.util.ArrayList;
 
 public class Field extends ArrayList<CropRow> {
 
-    public int getFieldSize() {
-        return fieldSize;
-    }
-
-    public void setFieldSize(int fieldSize) {
-        this.fieldSize = fieldSize;
-    }
-
-    private int fieldSize = 0;
 
 }
